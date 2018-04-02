a9a1780f81cd1817652661ea08fce4d7dd68c9e9
Jeff Wainwright <yowainwright@gmail.com>
2018-04-02 01:48:01 -0700

    [init] adds starter packages

---
+++ packages/heartly-readme-boilerplate/scripts/copy.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const { exec } = require('child_process')
+
+exec('cp files/DEFAULT_README.md README.md', (err, stdout, stderr) => {
+  if (err) {
+    console.error('Heartly Readme Boilerplate error when copying DEFAULT_README.md. See error below...')
+    console.error(err.stack)
+    console.error(stdout.toString())
+    console.error(stderr.toString())
+    console.error('Please let the Heartly crew know at https://github.com/heartly. Heartly Readme Boilerplate exiting...')
+    process.exit(1)
+  }
+})
