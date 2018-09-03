9adab18da5f12b8a0073232f58e49a2f2dd64153
Jeff Wainwright <yowainwright@gmail.com>
2018-09-02 20:48:02 -0700

    [init] adds initial values

---
+++ packages/heartly-cli/lib/selections.js
@@ -0,0 +1,32 @@
+exports.features = [
+  {
+    message: 'The Code of Conduct provides information about how to contribute to a repository',
+    name: 'code-of-conduct',
+    options: ['default'],
+  },
+  {
+    message: 'Codeowner files (`CODEOWNERS`) provide an easier way to assign code reviews',
+    name: 'codeowners',
+    options: ['default'],
+  },
+  {
+    message: 'Script are commands that can be run via npm to do something, like lint or test',
+    name: 'npm-scripts',
+    options: ['default'],
+  },
+  {
+    message: 'Github Templates are use to communicate to developers what is expected for code reviews and issues',
+    name: 'github-templates',
+    options: ['default'],
+  },
+  {
+    message: 'Readmes provide information developers about what a repository is and what it does',
+    name: 'readme',
+    options: ['default'],
+  },
+  {
+    message: 'Continuous integration helps developers test code',
+    name: 'continuous-integration',
+    options: ['default'],
+  },
+]
