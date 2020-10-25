2ef04b1091a8384f21e562ac7d2339fb4cde90ad
yowainwright <yowainwright@gmail.com>
2020-10-24 21:38:51 -0700

    fix(root): rename heartly-cli, eslint updates

---
+++ packages/heartly-cli/src/ui.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { Text } from 'ink'
+
+export const App: FC<{ name?: string }> = ({ name = 'Stranger' }) => (
+  <Text>
+    Hello, <Text color='green'>{name} 💕</Text>
+  </Text>
+)
+
+export default App
