327c0807d04676e8836311a9184147d91f9d2d9e
yowainwright <yowainwright@gmail.com>
2022-03-25 14:35:08 -0700

    feat(root): fixes linting issue

--- packages/eslint-config/src/script.ts
+++ packages/eslint-config/src/script.ts
@@ -1,20 +1,9 @@
-const { join, basename, dirname, resolve } = require('path')
-const fs = require('fs')
+const { resolve } = require('path')
+const { copyFile } = require('fs')
 
-// grab sibling to script.ts/js
-const config = resolve(__dirname, '../config/.eslintrc')
-const dest = `${__dirname}/.eslintrc`
-console.log({
-  resolve2: resolve(__dirname, '../config/.eslintrc'),
-  __dirname, // returns the folder path from where the function is being invoked
-  config, // uses join(__dirname, '<path-to-relevant-file-relative-to-the-script-being-invoked>'), ie config/.eslintrc
-  dest,
-  resolutionTest: resolve('config/.eslintrc'),
-  dirNameTest: dirname('config/.eslintrc'),
-  baseNameTest: basename('config/.eslintrc'),
-})
-fs.copyFile(config, dest, (err) => {
-  if (err) {
-    console.log(`There was an error copying data, ${err}, ${config}, ${__dirname}`)
-  }
+const config = resolve(__dirname, '../config/.template-eslintrc')
+const dest = resolve(__dirname, '.eslintrc')
+
+copyFile(config, dest, (err) => {
+  if (err) console.log(`There was an error copying data, ${err}, ${config}, ${__dirname}`)
 })
