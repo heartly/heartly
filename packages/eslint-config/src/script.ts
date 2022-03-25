f741331346b016cbf226d2ba6f489e9849c00ded
yowainwright <yowainwright@gmail.com>
2022-03-25 14:51:32 -0700

    fix(eslint-config): update script

--- packages/eslint-config/src/script.ts
+++ packages/eslint-config/src/script.ts
@@ -4,6 +4,5 @@ const { copyFile } = require('fs')
 const config = resolve(__dirname, '../config/.template-eslintrc')
 const dest = resolve(__dirname, '.eslintrc')
 
-copyFile(config, dest, (err) => {
-  if (err) console.log(`There was an error copying data, ${err}, ${config}, ${__dirname}`)
-})
+// only log if error
+copyFile(config, dest, (err) => err && console.log(`There was an error copying data, ${err}, ${config}, ${__dirname}`))
